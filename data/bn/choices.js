9029e528aef4d417b9642474612fd59df66eb9d1
Punit Singh <thepunitsingh@gmail.com>
2019-04-25 11:19:52 +0530

    Added Bangali

---
+++ data/bn/choices.js
@@ -0,0 +1,56 @@
+module.exports = {
+  plus: [
+    {
+      text: 'খুবই বেঠিক',
+      score: 1,
+      color: 1
+    },
+    {
+      text: 'পরিমিতরূপে বেঠিক',
+      score: 2,
+      color: 2
+    },
+    {
+      text: 'না সঠিক না বেঠিক',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'পরিমিতরূপে সঠিক',
+      score: 4,
+      color: 4
+    },
+    {
+      text: 'খুবই সঠিক',
+      score: 5,
+      color: 5
+    }
+  ],
+  minus: [
+    {
+      text: 'খুবই বেঠিক',
+      score: 5,
+      color: 1
+    },
+    {
+      text: 'পরিমিতরূপে বেঠিক',
+      score: 4,
+      color: 2
+    },
+    {
+      text: 'না সঠিক না বেঠিক',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'পরিমিতরূপে সঠিক',
+      score: 2,
+      color: 4
+    },
+    {
+      text: 'খুবই সঠিক',
+      score: 1,
+      color: 5
+    }
+  ]
+}
